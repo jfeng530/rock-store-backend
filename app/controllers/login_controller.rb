51cb502d686d7b049b1c0ab0b7070bc7e6e5a8a6
github username <github email address>
2019-12-05 20:26:23 -0500

    changes

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -3,7 +3,7 @@ class LoginController < ApplicationController
     def create
       user = User.find_by("lower(username) = ?", params[:username].downcase)
       if user && user.authenticate(params[:password])
-        render json: { token: token(user.id), total: user.orders.last.showTotal, purchases: user.orders.last.showPurchaseRocks, user_id: user.id, order_id: user.orders.last.id }
+        render json: { token: token(user.id), total: user.orders.last.showTotal, purchases: user.orders.last.purchases, user_id: user.id, order_id: user.orders.last.id }
       else
         render json: { errors: [ "That didn't match any users WE know about 💁" ] }, status: :unprocessable_entity
       end 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
     def create
         user = User.create(user_params)
         if user.valid?
-        render json: { token: token(user.id), user_id: user.id}
+        render json: { token: token(user.id), user_id: user.id }
         
         else
         render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
